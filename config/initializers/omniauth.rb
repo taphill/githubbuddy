cf8a2f7090d2315bd4a2ea665d4b1bca8e9f6d40
Taylor Phillips <taylorscottphillips@gmail.com>
2020-11-28 12:55:46 -0600

    Add omniauth initializer

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :github, Rails.application.credentials.github[:client_id], Rails.application.credentials.github[:client_secret]
+end
