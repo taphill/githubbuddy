b88b0360514ae947140747e5487a106bfc038089
Taylor Phillips <taylorscottphillips@gmail.com>
2020-11-28 16:45:08 -0600

    Index uid and nickname in Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_28_211653) do
+ActiveRecord::Schema.define(version: 2020_11_28_222952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2020_11_28_211653) do
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["nickname"], name: "index_users_on_nickname", unique: true
+    t.index ["uid"], name: "index_users_on_uid", unique: true
   end
 
 end
