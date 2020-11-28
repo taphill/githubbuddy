c61ddf48c5b2fa2dabe3e7f5029f428092f7a3ba
Taylor Phillips <taylorscottphillips@gmail.com>
2020-11-28 15:11:54 -0600

    Add validations to user spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    subject { FactoryBot.build(:user) }
+
+    it { should validate_presence_of(:uid) }
+    it { should validate_uniqueness_of(:uid).case_insensitive }
+
+    it { should validate_presence_of(:token) }
+    it { should validate_uniqueness_of(:token) }
+
+    it { should validate_presence_of(:nickname) }
+    it { should validate_uniqueness_of(:nickname) }
+
+    it { should validate_presence_of(:name) }
+  end
+end
