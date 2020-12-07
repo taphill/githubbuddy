bb0b93a8637497c166b330c698f1489b5d8da5e9
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-06 19:33:17 -0600

    Add feature test for users/index

---
+++ spec/features/users/index_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "Users/Index", type: :feature do
+  describe 'as a authenticated user' do
+    context 'when I visit the users index' do
+      before do
+        visit '/stars'
+      end
+
+      it { expect(page).to have_content('Starred Repos') }
+    end
+  end
+end
