b8cc8515e57676a4c277075458d1f25a37eda07b
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-07 21:08:08 -0600

    Add permanent cookie for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by(auth_token: cookies[:auth_token]) if cookies[:auth_token]
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,9 @@
 class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(request.env['omniauth.auth'])
-    session[:user_id] = user.id
+    # session[:user_id] = user.id
+    cookies.permanent[:auth_token] = user.auth_token
     flash[:notice] = "Hello #{user.nickname}!"
     redirect_to root_path
-
-    # auth_hash = request.env['omniauth.auth']
-    # res = Faraday.get("https://api.github.com/users/#{auth_hash['info']['nickname']}/starred")
-    # starred_repos = JSON.parse(res.body, symbolize_names: true)
   end
 end
