6133e19e66e196cfdcfb848209924b8a6735b8b8
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-08 10:24:39 -0600

    Update rails_helper config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -83,4 +83,8 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
+  config.configure_rspec_metadata!
+  config.filter_sensitive_data('<GITHUB_CLIENT_ID>') { Rails.application.credentials.github[:client_id] }
+  config.filter_sensitive_data('<GITHUB_CLIENT_SECRET>') { Rails.application.credentials.github[:client_secret] }
+  config.default_cassette_options = { re_record_interval: 7.days }
 end
