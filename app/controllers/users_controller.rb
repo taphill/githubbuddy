c28d1c2d952cd360d2aa35dcec8a03f3d2882780
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-08 18:48:25 -0600

    wip: Refactor users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def index
     redirect_to root_path unless current_user
+
+    @repos = current_user.get_stars.map do |repo_data|
+      Repo.new(repo_data)
+    end
   end
 end
