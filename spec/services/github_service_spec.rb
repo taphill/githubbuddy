762ab27ebe31e69fe4a95f1eb817a0f8e5f43287
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-09 08:39:20 -0600

    Add expectations to github service spec

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -6,7 +6,15 @@ RSpec.describe GitHubService do
       it "returns a user's starred repos" do
         VCR.use_cassette('github_service_spec') do
           repos = GitHubService.user_stars('taphill') 
-require 'pry'; binding.pry
+          repo_data = repos.first
+
+          expect(repo_data[:owner]).to have_key :login
+          expect(repo_data).to have_key :name
+          expect(repo_data).to have_key :html_url
+          expect(repo_data).to have_key :description
+          expect(repo_data).to have_key :language
+          expect(repo_data).to have_key :stargazers_count
+          expect(repo_data).to have_key :forks_count
         end
       end
     end
