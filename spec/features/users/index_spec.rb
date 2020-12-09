da914d02ca7b6b8e4cabdddac29043ed40cbe7ad
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-09 09:13:58 -0600

    Add test for user/index view

--- spec/features/users/index_spec.rb
+++ spec/features/users/index_spec.rb
@@ -1,4 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Users/Index", type: :feature do
+  describe 'As an authenticated user' do
+    context 'When I visit the user index page' do
+      before do
+        login_user
+      end
+
+      it 'has repo details' do
+        VCR.use_cassette('user_starred_repos') do
+          visit stars_path
+          
+          expect(page).to have_link('junegunn / fzf')
+        end
+      end
+    end
+  end
 end
