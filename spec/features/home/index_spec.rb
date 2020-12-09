8adf25e531b17d9e3112e55711e72bcba11bddad
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-09 10:33:19 -0600

    Add sad path test for unsuccessful sign in

--- spec/features/home/index_spec.rb
+++ spec/features/home/index_spec.rb
@@ -3,38 +3,42 @@
 require 'rails_helper'
 
 RSpec.describe 'Home/Index', type: :feature do
-  describe 'login' do
-    before do
-      OmniAuth.config.mock_auth[:github] = nil
-      OmniAuth.config.mock_auth[:github] = {
-        'provider' => 'github',
-        'uid' => '1285456',
-        'info' => {
-          'nickname' => 'adoug',
-          'email' => 'adouglas@example.com',
-          'name' => 'Aditya Douglas',
-          'image' => 'https://static.thenounproject.com/png/340719-200.png'
-        },
-        'credentials' => {
-          'token' => 'e596f957145e7b9c14',
-          'expires' => 'false'
+  describe 'Sign in' do
+    context 'When successful' do
+      before do
+        OmniAuth.config.mock_auth[:github] = nil
+        OmniAuth.config.mock_auth[:github] = {
+          'provider' => 'github',
+          'uid' => '1285456',
+          'info' => {
+            'nickname' => 'adoug',
+            'email' => 'adouglas@example.com',
+            'name' => 'Aditya Douglas',
+            'image' => 'https://static.thenounproject.com/png/340719-200.png'
+          },
+          'credentials' => {
+            'token' => 'e596f957145e7b9c14',
+            'expires' => 'false'
+          }
         }
-      }
 
-      visit root_path
-      click_link 'Sign In'
-    end
+        visit root_path
+        click_link 'Sign In'
+      end
 
-    context 'when successful' do
+      it { expect(page).to have_current_path(root_path) }
       it { expect(page).to have_content('Hello adoug!') }
     end
 
-    context 'when unsuccessful' do
+    context 'When unsuccessful' do
       before do
         OmniAuth.config.mock_auth[:github] = :invalid_credentials
+        visit root_path
+        click_link 'Sign In'
       end
       
       it { expect(page).to have_current_path(root_path) }
+      it { expect(page).to have_content('Failed login') }
     end
   end
 end
