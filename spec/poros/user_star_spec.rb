bc83f2c8cc21d7e261df3c5b512fbf8f38db16e7
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-11 19:15:23 -0600

    Refactor user star poro spec

--- spec/poros/user_star_spec.rb
+++ spec/poros/user_star_spec.rb
@@ -3,14 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe UserStar do
-  let(:user_star) { described_class.new(mock_repo_data) }
+  # let(:user_star) { described_class.new(mock_repo_data) }
+  let(:attr) do
+    {
+      name: 'mikejones',
+      owner: { login: 'Mike Jones' },
+      html_url: 'https://github.com/mikejones/iceage',
+      description: 'Ice age, ice age...',
+      language: 'Ruby',
+      stargazers_count: 1_000_000,
+      forks_count: 500_000
+    }
+  end
+
+  let(:user_star) { UserStar.new(attr) }
 
   it { expect(user_star).to be_a described_class }
-  it { expect(user_star.name).to eq('travis.rb') }
-  it { expect(user_star.owner).to eq('travis-ci') }
-  it { expect(user_star.url).to eq('https://github.com/travis-ci/travis.rb') }
-  it { expect(user_star.description).to eq(mock_repo_data[:description]) }
+  it { expect(user_star.name).to eq('mikejones') }
+  it { expect(user_star.owner).to eq('Mike Jones') }
+  it { expect(user_star.url).to eq('https://github.com/mikejones/iceage') }
+  it { expect(user_star.description).to eq('Ice age, ice age...') }
   it { expect(user_star.language).to eq('Ruby') }
-  it { expect(user_star.star_count).to eq(1525) }
-  it { expect(user_star.fork_count).to eq(415) }
+  it { expect(user_star.star_count).to eq(1_000_000) }
+  it { expect(user_star.fork_count).to eq(500_000) }
 end
