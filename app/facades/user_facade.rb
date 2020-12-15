5660832eb3ed1a7dd379aae59ec93fa191381ae3
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-15 00:33:15 -0600

    Add create_repo_records method to user facade

--- app/facades/user_facade.rb
+++ app/facades/user_facade.rb
@@ -1,17 +1,22 @@
 # frozen_string_literal: true
 
 class UserFacade
-  def self.user_stars(user_nickname)
-    json = GitRepoService.user_stars(user_nickname)
+  def self.user_stars(user)
+    json = GitRepoService.user_stars(user.nickname)
 
-    stars = json.map do |star_data|
+    create_repo_records(user: user, json_data: json)
+
+    json.map do |star_data|
       UserStar.new(star_data)
     end
+  end
 
+  def self.create_repo_records(user:, json_data:)
+    json_data.each do |star_data|
+      repo = Repo.find_or_create_by(github_repo_id: star_data[:id])
 
-
-    Repo.find_or_create_by(github_repo_id: ) do
-      
+      user_repo = UserRepo.new(user_id: user.id, repo_id: repo.id)
+      user_repo.save
     end
   end
 end
