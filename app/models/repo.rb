00217786937208d5b281a6b19edfcf7532024cde
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-19 17:18:05 -0600

    Add new validations to Repo model

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class Repo < ApplicationRecord
-  validates :github_repo_id, presence: true, uniqueness: true
+  validates :github_id, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :owner, presence: true
+  validates :url, presence: true
 
   has_many :user_repos
   has_many :users, through: :user_repos
