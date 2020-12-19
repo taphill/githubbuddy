0857a1eb46acf64e9982772f5962c3e9e436133d
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-19 00:05:18 -0600

    Rename UserRepo conrtoller to Tags controller

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class TagsController < ApplicationController
+  def edit
+    require 'pry'; binding.pry
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get '/:nickname', to: 'users#index', as: 'user_root'
 
-  get '/tags/:repo', to: 'user_repos#edit', as: 'user_repo_edit'
+  get '/tags/:repo', to: 'tags#edit', as: 'tags_edit'
   # patch '/:repo', to: 'user_repos#update', as: 'user_repo'
 
   get 'auth/github/callback', to: 'sessions#create'
