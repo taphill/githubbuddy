5a1c9da4c907a35ed86253b649e5dd2712dde7ac
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-19 17:13:39 -0600

    Add index action and view for Welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@
 Rails.application.routes.draw do
   root 'home#index'
 
+  resources :welcome, only: [:index]
+
+  resources :sync, only: [:index]
+
   get '/:nickname', to: 'users#index', as: 'user_root'
 
   get '/tags/:repo', to: 'tags#edit', as: 'tags_edit'
-  # patch '/:repo', to: 'user_repos#update', as: 'user_repo'
 
   get 'auth/github/callback', to: 'sessions#create'
   get 'auth/failure', to: 'auth#failure' 
