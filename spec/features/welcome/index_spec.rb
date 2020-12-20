3e244970eb7730fb21f43636a3ce1e5bc4cac547
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-20 13:27:00 -0600

    Refactor welcome/index spec

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -8,11 +8,28 @@ RSpec.describe 'Welcome/Index', type: :feature do
       login_user
     end
 
-    it 'fetchs a users stars', :vcr do
-      visit welcome_index_path
-      click_link 'Sync Account'
+    it 'takes user to the user/index' do
+      VCR.use_cassette('Welcome/Index') do
+        visit welcome_index_path
+        click_link 'Sync Account'
 
-      expect(page).to have_current_path(user_root_path('taphill'))
+        expect(page).to have_current_path(user_root_path('taphill'))
+      end
+    end
+
+    it 'fetchs all user stars' do
+      VCR.use_cassette('Welcome/Index') do
+        visit welcome_index_path
+        click_link 'Sync Account'
+
+        expect(page).to have_css('.user-star', count: 67)
+
+        within(all('.user-star').last) do
+          expect(page).to have_link('soul-lang / SOUL')
+          expect(page).to have_content('The SOUL programming language and API')
+          expect(page).to have_link('Add tags')
+        end
+      end
     end
   end
 end
