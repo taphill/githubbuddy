f03908b18b823c026c49615efa7db2225b99c2ff
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-19 19:38:25 -0600

    Rename UserFacade spec to SyncFacade

--- app/facades/sync_facade.rb
+++ app/facades/sync_facade.rb
@@ -1,22 +1,23 @@
 # frozen_string_literal: true
 
 class SyncFacade
-  def self.create_repo_records_for(user)
+  def self.fetch_user_stars(user)
     json_data = GitRepoService.user_stars(user.nickname)
 
     json_data.each do |data|
-      next if Repo.find_by(github_repo_id: data[id])
-
-      repo = Repo.create(
-        github_repo_id: data[:id],
-        name: data[:name],
-        owner: data[:owner][:login],
-        url: data[:html_url],
-        description: data[:description],
-      )
+      repo = find_or_create_repo(data)
 
       user_repo = UserRepo.new(user_id: user.id, repo_id: repo.id)
       user_repo.save
+    end
+  end
+
+  def self.find_or_create_repo(data)
+    Repo.find_or_create_by(github_id: data[:id]) do |repo|
+      repo.name = data[:name]
+      repo.owner = data[:owner][:login]
+      repo.url = data[:html_url]
+      repo.description = data[:description]
     end
   end
 end
