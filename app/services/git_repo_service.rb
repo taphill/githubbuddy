895279e927a61a40be4abc5afd677aa3ff06c1ef
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-19 23:24:11 -0600

    Refactor git repo service

--- app/services/git_repo_service.rb
+++ app/services/git_repo_service.rb
@@ -1,23 +1,13 @@
 # frozen_string_literal: true
 
 class GitRepoService
-  def self.conn
-    Faraday.new(url: 'https://api.github.com')
-  end
-
   def self.user_stars(user_nickname)
     stars = []
     page_number = 0
 
     loop do
       page_number += 1
-
-      response = conn.get("/users/#{user_nickname}/starred") do |req|
-        req.params['page'] = page_number
-      end
-
-      json = JSON.parse(response.body, symbolize_names: true)
-
+      json = stars_response(user_nickname: user_nickname, page_number: page_number)
       break if json.empty?
 
       stars << json
@@ -25,4 +15,19 @@ class GitRepoService
 
     stars.flatten
   end
+
+  def self.conn
+    Faraday.new(url: 'https://api.github.com')
+  end
+
+  def self.stars_response(user_nickname:, page_number:)
+    response = conn.get("/users/#{user_nickname}/starred") do |req|
+      req.params['page'] = page_number
+      req.params['per_page'] = 100
+    end
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private_class_method :conn, :stars_response
 end
