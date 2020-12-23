4aeccd485f1141b06cdeffe7424e1e0d9a12b266
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-23 12:12:34 -0600

    Update user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,17 @@ FactoryBot.define do
   factory :user do
     sequence(:github_id) { |n| "100#{n}" }
     nickname { Faker::Internet.username }
+    image { 'https://thumbs.dreamstime.com/b/no-image-available-icon-flat-vector-no-image-available-icon-flat-vector-illustration-132482930.jpg' }
+    auth_token { Faker::Number.number(digits: 10) }
+
+    trait :with_repos do
+      transient do
+        repo_count { 3 }
+      end
+
+      after(:create) do |user, evaluator|
+        user.repos << create_list(:repo, evaluator.repo_count)
+      end
+    end
   end
 end
