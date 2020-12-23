2d60153523ae812e7e51dc9f9c8feb0dd7682e18
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-23 15:43:41 -0600

    Add tags controller new action and view

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,31 @@
 # frozen_string_literal: true
 
 class TagsController < ApplicationController
+  def new
+    @repo = Repo.find(params[:repo_id])
+    @user_repo = UserRepo.find_by(user_id: current_user.id, repo_id: params[:repo_id])
+  end
+
+  def create
+    tags = tag_params[:tags].split(/\s*,\s*/)
+
+    tags.each do |tag|
+      x = Tag.find_or_create_by!(name: tag.downcase)
+
+      user_tag = Tagging.new(tag_id: x.id, user_repo_id: tag_params[:user_repo_id])
+      user_tag.save
+    end
+
+    redirect_to user_root_path(current_user.nickname) 
+  end
+
   def edit
     @user_star = UserRepo.find_by(user_id: current_user.id, repo_id: params[:repo_id])
+  end
+
+  private
+
+  def tag_params
+    params.permit(:tags, :user_repo_id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@
 
 class UsersController < ApplicationController
   def index
+require 'pry'; binding.pry
     @stars = current_user.repos
   end
 end
