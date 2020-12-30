008c8691ab080d1b5402049e92ad497b328b8ec6
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-29 18:36:04 -0600

    Add user_stars method to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,16 @@
 
 class UsersController < ApplicationController
   def index
-    @stars = current_user.repos
+    @stars = user_stars
+  end
+
+  private
+
+  def user_stars
+    if params[:tag]
+      current_user.repos_with_tag(params[:tag])
+    else
+      current_user.repos
+    end
   end
 end
