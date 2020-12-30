e7837645ae58ebbb0e0edc6808c5acb1f164b261
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-29 23:14:53 -0600

    Add search functionality to users index view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,9 @@ class UsersController < ApplicationController
   private
 
   def user_stars
-    if params[:tag]
-      current_user.repos_with_tag(params[:tag])
-    else
-      current_user.repos
-    end
+    return Repo.search(query: params[:query], user_id: current_user.id) if params[:query]
+    return current_user.repos_with_tag(params[:tag]) if params[:tag]
+
+    current_user.repos
   end
 end
