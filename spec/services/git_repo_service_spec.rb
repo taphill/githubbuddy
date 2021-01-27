f3b26013ac5a2368eed9d5bcc14f97c1bc3f74f7
Taylor Phillips <taylorscottphillips@gmail.com>
2021-01-27 12:17:30 -0600

    Add spec for .latest_release method

--- spec/services/git_repo_service_spec.rb
+++ spec/services/git_repo_service_spec.rb
@@ -25,5 +25,26 @@ RSpec.describe GitRepoService, type: :service do
       it { expect(first_result).to have_key :description }
       it { expect(first_result[:description]).to be_a(String) }
     end
+
+    describe '.latest_release(owner, repo)', :vcr do
+      let(:result) { described_class.latest_release(owner: 'Homebrew', repo: 'brew') }
+
+      it { expect(result).to be_a(Hash) }
+
+      it { expect(result).to have_key(:html_url) }
+      it { expect(result[:html_url]).to be_a(String) }
+
+      it { expect(result).to have_key(:tag_name) }
+      it { expect(result[:tag_name]).to be_a(String) }
+
+      it { expect(result).to have_key(:created_at) }
+      it { expect(result[:created_at]).to be_a(String) }
+
+      it { expect(result).to have_key(:published_at) }
+      it { expect(result[:published_at]).to be_a(String) }
+
+      it { expect(result).to have_key(:body) }
+      it { expect(result[:body]).to be_a(String) }
+    end
   end
 end
