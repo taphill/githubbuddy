f1d3f216eb4b24a5575203367a5de8899759e783
Taylor Phillips <taylorscottphillips@gmail.com>
2021-01-27 12:54:49 -0600

    Add ReleaseFacade spec

---
+++ spec/facades/release_facade_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe ReleaseFacade, type: :facade do
+  describe 'class methods' do
+    describe '.fetch_latest_release(owner, repo)' do
+      context 'when there is a latest release', :vcr do
+        let(:release) { ReleaseFacade.fetch_latest_release(owner: 'Homebrew', repo: 'brew') }
+
+        it { expect(release).to be_a(Release) }
+
+        it { expect(release.url).to be_a(String) }
+        it { expect(release.tag_name).to be_a(String) }
+        it { expect(release.created_at).to be_a(String) }
+        it { expect(release.published_at).to be_a(String) }
+        it { expect(release.body).to be_a(String) }
+      end
+
+      context 'when no release exists', :vcr do
+        let(:release) { ReleaseFacade.fetch_latest_release(owner: 'taphill', repo: 'graph_ql') }
+
+        it { expect(release).to be_nil }
+      end
+    end
+  end
+end
