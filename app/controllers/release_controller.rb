cdde88d7809ea951c97b1a99720824778c752800
Taylor Phillips <taylorscottphillips@gmail.com>
2021-01-27 20:40:45 -0600

    Update release index action

--- app/controllers/release_controller.rb
+++ app/controllers/release_controller.rb
@@ -1,5 +1,10 @@
+require 'redcarpet'
+
 class ReleaseController < ApplicationController
   def index
     @release = ReleaseFacade.fetch_latest_release(owner: params[:owner], repo: params[:repo])  
+    @owner = params[:owner]
+    @repo = params[:repo]
+    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, extensions = {})
   end
 end
