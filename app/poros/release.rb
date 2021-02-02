a103f53756a8399f7033b7a21c5eb0b0eb25c527
Taylor Phillips <taylorscottphillips@gmail.com>
2021-02-02 16:38:52 -0600

    Update Release poro

--- app/poros/release.rb
+++ app/poros/release.rb
@@ -6,8 +6,22 @@ class Release
   def initialize(attr)
     @url = attr[:html_url]
     @tag_name = attr[:tag_name]
-    @created_at = Date.strptime(attr[:created_at]).strftime('%B %d, %Y')
-    @published_at = Date.strptime(attr[:published_at]).strftime('%B %d, %Y')
+    @created_at = release_created_at(attr[:created_at])
+    @published_at = release_published_at(attr[:published_at])
     @body = attr[:body]
+  end
+
+  private
+  
+  def release_created_at(data)
+    return nil unless data
+
+    Date.strptime(data).strftime('%B %d, %Y')
+  end
+
+  def release_published_at(data)
+    return nil unless data
+
+    Date.strptime(data).strftime('%B %d, %Y')
   end
 end
