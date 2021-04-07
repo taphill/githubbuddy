f025e391af28939132094d96ecb1477a391076fa
Taylor Phillips <taylorscottphillips@gmail.com>
2021-04-07 18:11:31 -0500

    Remove purgecss config from postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,28 +1,13 @@
-let environment = {
+module.exports = {
   plugins: [
+    require('tailwindcss')("./app/javascript/stylesheets/tailwind.config.js"),
     require('postcss-import'),
-    require('tailwindcss')('./app/javascript/stylesheets/tailwind.config.js'),
-    require('autoprefixer'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009'
       },
       stage: 3
-    }),
+    })
   ]
 }
-
-if (process.env.RAILS_ENV === "production") {
-  environment.plugins.push(
-    require('@fullhuman/postcss-purgecss')({
-      content: ['./app/**/*.html.erb', '.app/helper/*.rb'],
-      safelist: {
-        greedy: [/input|prose$/],
-      },
-      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-    })
-  )
-}
-
-module.exports = environment
