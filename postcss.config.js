174b214c195b935cd0d35223d7fc286acbef6bf9
Taylor Phillips <taylorscottphillips@gmail.com>
2021-04-07 19:15:41 -0500

    Update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
-module.exports = {
+let environment = {
   plugins: [
     require('tailwindcss')("./app/javascript/stylesheets/tailwind.config.js"),
+    require('autoprefixer'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
@@ -8,6 +9,20 @@ module.exports = {
         flexbox: 'no-2009'
       },
       stage: 3
-    })
+    }),
   ]
 }
+
+if (process.env.RAILS_ENV === "production") {
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: ['./app/**/*.html.erb', '.app/helper/*.rb'],
+      safelist: {
+        greedy: [/input|prose$/],
+      },
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+  )
+}
+
+module.exports = environment
